944990da7f2af9e9f09e7303c4d955d3a104fcbb
ujiik009 <ujiik009@hotmail.com>
2018-02-04 13:01:30 +0700

    update

--- app/src/main/java/followoftheking/ionic/io/authen/MainActivity.java
+++ app/src/main/java/followoftheking/ionic/io/authen/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
